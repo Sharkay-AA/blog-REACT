42a98490845f3c37745c00b9c175e832c913553c
Jason Lavigne <jasonlavigne824@gmail.com>
2022-03-06 12:55:42 +0100

    store,articleReducer created

---
+++ src/redux/articles/articleReducer.js
@@ -0,0 +1,17 @@
+const INITIAL_STATE = {
+    articles: []
+};
+
+function articleReducer(state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case "LOADARTICLES": {
+            return {
+                ...state,
+                articles: action.payload
+            }
+        }
+    }
+    return state;
+};
+
+export default articleReducer;
