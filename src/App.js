2784eb2fd25f717836a0c799db31218aff9e509b
Jason Lavigne <jasonlavigne824@gmail.com>
2022-03-06 12:22:03 +0100

    complete responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
-import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Navbar />
     </div>
   );
 }
