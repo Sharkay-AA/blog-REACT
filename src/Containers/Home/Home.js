f862110233f63fbeec8ca8b81954065b96320594
Jason Lavigne <jasonlavigne824@gmail.com>
2022-03-06 12:40:51 +0100

    complete responsive Card component, add Card Component to Home container

---
+++ src/Containers/Home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Home.css";
+import Card from "../../Components/Card/Card";
+
+function Home(props) {
+    return (
+        <>
+            <h1 className='home-title'>Tous les articles</h1>
+            <div className="container-cards">
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+                <Card>
+                    <h2>Hello Card</h2>
+                </Card>
+            </div>
+        </>
+    );
+}
+
+export default Home;
