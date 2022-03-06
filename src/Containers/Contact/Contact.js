28303531ed4b631d670c380b48adb00011084bed
Jason Lavigne <jasonlavigne824@gmail.com>
2022-03-06 14:51:24 +0100

    add very simple contact page

--- src/Containers/Contact/Contact.js
+++ src/Containers/Contact/Contact.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import "./Contact.css";
 
 function Contact(props) {
     return (
-        <div>
+        <div className='container-contact'>
+            <h1>Contactez-nous</h1>
+            <p>Par mail : zzz@eee</p>
+            <p>Par téléphone : XX-XX-XX</p>
+            <p>Rejoignez-nous sur les réseaux :</p>
+            <ul>
+                <li>Facebook</li>
+                <li>Twitter</li>
+                <li>LinkedIn</li>
+            </ul>
 
         </div>
     );
